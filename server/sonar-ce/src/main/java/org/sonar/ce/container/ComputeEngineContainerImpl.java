aa03e7e081fe8baf59cf074a5376f304432f0950
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-11 17:04:29 +0100

    SONAR-6732 add components from PlatformLevel3 to CE container

--- server/sonar-ce/src/main/java/org/sonar/ce/container/ComputeEngineContainerImpl.java
+++ server/sonar-ce/src/main/java/org/sonar/ce/container/ComputeEngineContainerImpl.java
@@ -23,6 +23,7 @@ import com.google.common.annotations.VisibleForTesting;
 import java.util.List;
 import org.sonar.api.utils.Durations;
 import org.sonar.api.utils.System2;
+import org.sonar.api.utils.UriReader;
 import org.sonar.core.config.CorePropertyDefinitions;
 import org.sonar.core.i18n.DefaultI18n;
 import org.sonar.core.i18n.RuleI18nManager;
@@ -30,6 +31,7 @@ import org.sonar.core.platform.ComponentContainer;
 import org.sonar.core.platform.Module;
 import org.sonar.core.platform.PluginClassloaderFactory;
 import org.sonar.core.platform.PluginLoader;
+import org.sonar.core.util.DefaultHttpDownloader;
 import org.sonar.core.util.UuidFactoryImpl;
 import org.sonar.db.DaoModule;
 import org.sonar.db.DatabaseChecker;
@@ -44,6 +46,8 @@ import org.sonar.server.issue.index.IssueIndex;
 import org.sonar.server.platform.DatabaseServerCompatibility;
 import org.sonar.server.platform.DefaultServerFileSystem;
 import org.sonar.server.platform.DefaultServerUpgradeStatus;
+import org.sonar.server.platform.PersistentSettings;
+import org.sonar.server.platform.ServerIdGenerator;
 import org.sonar.server.platform.ServerImpl;
 import org.sonar.server.platform.ServerSettings;
 import org.sonar.server.platform.TempFolderProvider;
@@ -54,6 +58,7 @@ import org.sonar.server.plugins.ServerPluginRepository;
 import org.sonar.server.qualityprofile.index.ActiveRuleIndex;
 import org.sonar.server.rule.index.RuleIndex;
 import org.sonar.server.search.EsSearchModule;
+import org.sonar.server.startup.ServerMetadataPersister;
 
 public class ComputeEngineContainerImpl implements ComputeEngineContainer {
   private static final Object[] LEVEL_1_COMPONENTS = new Object[] {
@@ -111,6 +116,13 @@ public class ComputeEngineContainerImpl implements ComputeEngineContainer {
     RuleI18nManager.class, // used by DebtRulesXMLImporter
     Durations.class, // used in Web Services and DebtCalculator
   };
+  private static final Object[] LEVEL_3_COMPONENTS = new Object[] {
+    PersistentSettings.class,
+    ServerMetadataPersister.class,
+    DefaultHttpDownloader.class,
+    UriReader.class,
+    ServerIdGenerator.class
+  };
 
   private final ComponentContainer componentContainer;
 
@@ -125,7 +137,8 @@ public class ComputeEngineContainerImpl implements ComputeEngineContainer {
       .add(LEVEL_1_COMPONENTS)
       .add(toArray(CorePropertyDefinitions.all()))
       .add(toArray(CePropertyDefinitions.all()))
-      .add(LEVEL_2_COMPONENTS);
+      .add(LEVEL_2_COMPONENTS)
+      .add(LEVEL_3_COMPONENTS);
 
     configureFromModules();
 
